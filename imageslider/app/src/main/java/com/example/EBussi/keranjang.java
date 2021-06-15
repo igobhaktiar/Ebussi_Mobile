6b8ad0622d457dcfa0cfffbc9bb9da3561e97d5b
MLutfiF <71967862+MLutfiF@users.noreply.github.com>
2021-06-15 14:32:19 -0700

    Keranjang

---
+++ imageslider/app/src/main/java/com/example/EBussi/keranjang.java
@@ -0,0 +1,14 @@
+package com.example.EBussi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class keranjang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keranjang);
+    }
+}
